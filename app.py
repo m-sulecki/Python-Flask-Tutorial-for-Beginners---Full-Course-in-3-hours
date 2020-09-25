10d82769f6643af79de177f42afcf96ab5994a1b
MS <msulek59@hotmail.com>
2020-09-25 22:54:43 +0200

    from form to db works

--- app.py
+++ app.py
@@ -1,23 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+class BlogPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    author = db.Column(db.String(20), nullable=False, default = 'N/A')
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return 'Blog post ' + str(self.id)
+
 
 all_posts = [
-    {
-        'title': 'Post 1',
-        'content': 'This is the content of post 1.'
-    },
+    # {
+    #     'title': 'Post 1',
+    #     'content': 'This is the content of post 1.'
+    # },
 
-     {
-        'title': 'Post 2',
-        'content': 'This is the content of post 2.',
-        'author': 'Aaron',
-    },
+    #  {
+    #     'title': 'Post 2',
+    #     'content': 'This is the content of post 2.',
+    #     'author': 'Aaron',
+    # },
 
-     {
-        'title': 'Post 3',
-        'content': 'This is the content of post 3.'
-    },
+    #  {
+    #     'title': 'Post 3',
+    #     'content': 'This is the content of post 3.'
+    # },
 ]
 
 @app.route('/')
@@ -25,9 +40,19 @@ def index():
     # return "<h1>Home Page</h1>"
     return render_template('index.html')
 
-@app.route('/posts')
+@app.route('/posts', methods=['GET', 'POST'])
 def posts():
-    return render_template('posts.html', posts=all_posts)
+
+    if request.method == 'POST':
+        post_title = request.form['title']
+        post_content = request.form['content']
+        new_post = BlogPost(title=post_title, content=post_title, author='Aaron')
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        all_posts = BlogPost.query.order_by(BlogPost.date_posted).all()
+        return render_template('posts.html', posts=all_posts)
 
 @app.route('/')
 @app.route('/home/users/<string:name>/posts/<int:id>')
