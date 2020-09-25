16177d72aa72f7c0f6ca1d9de096549b5e0a62a0
MS <msulek59@hotmail.com>
2020-09-25 21:20:44 +0200

    first posts

--- app.py
+++ app.py
@@ -1,1 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+all_posts = [
+    {
+        'title': 'Post 1',
+        'content': 'This is the content of post 1.'
+    },
+
+     {
+        'title': 'Post 2',
+        'content': 'This is the content of post 2.',
+        'author': 'Aaron',
+    },
+
+     {
+        'title': 'Post 3',
+        'content': 'This is the content of post 3.'
+    },
+]
+
+@app.route('/')
+def index():
+    # return "<h1>Home Page</h1>"
+    return render_template('index.html')
+
+@app.route('/posts')
+def posts():
+    return render_template('posts.html', posts=all_posts)
+
+@app.route('/')
+@app.route('/home/users/<string:name>/posts/<int:id>')
+def hello(name, id):
+    return f"Helllo {name}! Your id is: {str(id)}"
+
+@app.route('/onlyget', methods=['GET'])
+def get_req():
+    return 'You can only get this webpage.'
+
+if __name__ == "__main__":
+    app.run(debug=True)
